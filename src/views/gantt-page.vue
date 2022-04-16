0d608fd36d92d95cc1d9a5096065c6673dd72973
Tatyana Matyzheva <darikapiterski@gmail.com>
2022-04-16 12:36:02 +0300

    first

---
+++ src/views/gantt-page.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <h2 class="content-block">Gantt Page</h2>
+    <div class="content-block">
+      <div class="dx-card responsive-paddings">
+        <DxGantt
+            :task-list-width="500"
+            :height="700"
+            scale-type="weeks"
+        >
+
+          <DxTasks :data-source="tasks"/>
+          <DxDependencies :data-source="dependencies"/>
+          <DxResources :data-source="resources"/>
+          <DxResourceAssignments :data-source="resourceAssignments"/>
+
+          <DxToolbar>
+            <DxItem name="undo"/>
+            <DxItem name="redo"/>
+            <DxItem name="separator"/>
+            <DxItem name="collapseAll"/>
+            <DxItem name="expandAll"/>
+            <DxItem name="separator"/>
+            <DxItem name="addTask"/>
+            <DxItem name="deleteTask"/>
+            <DxItem name="separator"/>
+            <DxItem name="zoomIn"/>
+            <DxItem name="zoomOut"/>
+          </DxToolbar>
+
+          <DxEditing :enabled="true"/>
+          <DxValidation :auto-update-parent-tasks="true"/>
+
+          <DxColumn
+              :width="300"
+              data-field="title"
+              caption="Subject"
+          />
+          <DxColumn
+              data-field="start"
+              caption="Start Date"
+          />
+          <DxColumn
+              data-field="end"
+              caption="End Date"
+          />
+        </DxGantt>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import {
+  DxGantt,
+  DxTasks,
+  DxDependencies,
+  DxResources,
+  DxResourceAssignments,
+  DxColumn,
+  DxEditing,
+  DxValidation,
+  DxToolbar,
+  DxItem,
+} from 'devextreme-vue/gantt';
+
+import {
+  tasks,
+  dependencies,
+  resources,
+  resourceAssignments,
+} from '/src/data';
+
+export default {
+  components: {
+    DxGantt,
+    DxTasks,
+    DxDependencies,
+    DxResources,
+    DxResourceAssignments,
+    DxColumn,
+    DxEditing,
+    DxValidation,
+    DxToolbar,
+    DxItem,
+  },
+  data() {
+    return {
+      tasks,
+      dependencies,
+      resources,
+      resourceAssignments,
+    };
+  },
+};
+</script>
+<style lang="scss">
+#gantt {
+  height: 700px;
+}
+</style>
