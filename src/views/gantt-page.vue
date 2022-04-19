da44ba967facfca66a5a6ce7e705001d90735448
Darika <darikapiterski@gmail.com>
2022-04-19 16:08:36 +0300

    two

--- src/views/gantt-page.vue
+++ src/views/gantt-page.vue
@@ -1,55 +1,50 @@
 <template>
-  <div>
-    <h2 class="content-block">Gantt Page</h2>
-    <div class="content-block">
-      <div class="dx-card responsive-paddings">
-        <DxGantt
-            :task-list-width="500"
-            :height="700"
-            scale-type="weeks"
-        >
+  <DxGantt
+      :task-list-width="500"
+      :height="700"
+      scale-type="weeks"
+  >
 
-          <DxTasks :data-source="tasks"/>
-          <DxDependencies :data-source="dependencies"/>
-          <DxResources :data-source="resources"/>
-          <DxResourceAssignments :data-source="resourceAssignments"/>
+    <DxTasks :data-source="tasks"/>
+    <DxDependencies :data-source="dependencies"/>
+    <DxResources :data-source="resources"/>
+    <DxResourceAssignments :data-source="resourceAssignments"/>
 
-          <DxToolbar>
-            <DxItem name="undo"/>
-            <DxItem name="redo"/>
-            <DxItem name="separator"/>
-            <DxItem name="collapseAll"/>
-            <DxItem name="expandAll"/>
-            <DxItem name="separator"/>
-            <DxItem name="addTask"/>
-            <DxItem name="deleteTask"/>
-            <DxItem name="separator"/>
-            <DxItem name="zoomIn"/>
-            <DxItem name="zoomOut"/>
-          </DxToolbar>
+    <DxToolbar>
+      <DxItem name="undo"/>
+      <DxItem name="redo"/>
+      <DxItem name="separator"/>
+      <DxItem name="collapseAll"/>
+      <DxItem name="expandAll"/>
+      <DxItem name="separator"/>
+      <DxItem name="addTask"/>
+      <DxItem name="deleteTask"/>
+      <DxItem name="separator"/>
+      <DxItem name="zoomIn"/>
+      <DxItem name="zoomOut"/>
+    </DxToolbar>
 
-          <DxEditing :enabled="true"/>
-          <DxValidation :auto-update-parent-tasks="true"/>
+    <DxEditing :enabled="true"/>
+    <DxValidation :auto-update-parent-tasks="true"/>
 
-          <DxColumn
-              :width="300"
-              data-field="title"
-              caption="Subject"
-          />
-          <DxColumn
-              data-field="start"
-              caption="Start Date"
-          />
-          <DxColumn
-              data-field="end"
-              caption="End Date"
-          />
-        </DxGantt>
-      </div>
-    </div>
-  </div>
+    <DxColumn
+        :width="300"
+        data-field="title"
+        caption="Subject"
+    />
+    <DxColumn
+        data-field="start"
+        caption="Start Date"
+    />
+    <DxColumn
+        data-field="end"
+        caption="End Date"
+    />
+  </DxGantt>
 </template>
 <script>
+import 'devexpress-gantt/dist/dx-gantt.min.js';
+
 import {
   DxGantt,
   DxTasks,
@@ -68,7 +63,7 @@ import {
   dependencies,
   resources,
   resourceAssignments,
-} from '/src/data';
+} from '/src/data.js';
 
 export default {
   components: {
@@ -93,7 +88,7 @@ export default {
   },
 };
 </script>
-<style lang="scss">
+<style>
 #gantt {
   height: 700px;
 }
